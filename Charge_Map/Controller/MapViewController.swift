834c9bdadfaceac889f83efd4fa2f682d698dfe2
po <nais95100@icloud.com>
2019-11-20 21:51:21 +0100

    - Organization of the project in MVC,
    - added controlerl that will manage the mapView,
    - added outlet.

---
+++ Charge_Map/Controller/MapViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MapViewController.swift
+//  Charge_Map
+//
+//  Created by Macbook pro on 20/11/2019.
+//  Copyright © 2019 Macbook pro. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    
+
+}
