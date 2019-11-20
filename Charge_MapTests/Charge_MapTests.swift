8629d50ea2ace524559032007f237f8f5f61c3db
Dylan <nais95100@icloud.com>
2019-11-20 21:32:15 +0100

    Initial Commit

---
+++ Charge_MapTests/Charge_MapTests.swift
@@ -0,0 +1,34 @@
+//
+//  Charge_MapTests.swift
+//  Charge_MapTests
+//
+//  Created by Macbook pro on 20/11/2019.
+//  Copyright © 2019 Macbook pro. All rights reserved.
+//
+
+import XCTest
+@testable import Charge_Map
+
+class Charge_MapTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
